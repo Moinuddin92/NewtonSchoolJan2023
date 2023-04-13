8eb3b6c0a9b0266de42d9e54fb469598d08ce1ca
Moinuddin Shavathi <mbsm_1992@yahoo.com>
2023-04-14 00:16:34 +0530

    Delete even nodes from the list - Linked List, Circular Linked List

---
+++ DeleteEven.java
@@ -0,0 +1,37 @@
+/*
+
+ class Node 
+{  
+    int data;  
+    Node next;  
+    Node(int x)  
+    {  
+        data = x;  
+        next = null;  
+    }  
+};
+*/
+static Node deleteEven(Node head){
+//Enter your code here
+    Node temp = head;
+    Node next;
+    do{
+            if(temp.data % 2 == 0){
+               deleteNode(head,temp);
+            }
+            next=temp.next;
+            temp=next;
+    }while(temp!=head);
+    return head;
+}
+static Node deleteNode(Node headRef, Node del){
+    Node temp = headRef;
+    if(headRef==del){
+        headRef=del.next;
+    }
+    while(temp.next!=del){
+        temp=temp.next;
+    }
+    temp.next=del.next;
+    return headRef;
+}
